133de21953e18b56cce433b55f9739c135573a44
paszosky <pavel.kowalczyk@gmail.com>
2025-11-04 23:00:36 +0100

    Wstep python 01: xyz

---
+++ xyz.py
@@ -0,0 +1,14 @@
+# weź od użytkownika dwie liczby i podaj ich sumę
+
+# weź od usera pierwszą liczbę
+print("Podaj pierwszą liczbę:")
+a = input()
+a = int(a)
+
+print("Podaj drugą liczbę:")
+b = input()
+b = int(b)
+
+c = a + b
+
+print(f"Wynik to: {c}")
