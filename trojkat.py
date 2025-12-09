7a3a4a243f2683bf3d90457031607906b267a180
paszosky <pavel.kowalczyk@gmail.com>
2025-12-09 22:59:42 +0100

    globals

--- trojkat.py
+++ trojkat.py
@@ -1,9 +1,24 @@
+prev_globals = set(globals().keys())
+def print_diff_globals():
+	global prev_globals
+	x = set(globals().keys())
+	print(x - prev_globals)
+	prev_globals = x
+
 def draw_triangle(n):
+	print("LOCALS", locals())
 	output = []
+	print("LOCALS", locals())
 	for i in range(n):
+		print("LOCALS", locals())
 		txt = "*" * (i + 1)
+		print("LOCALS", locals())
 		output.append(txt)
+	print("LOCALS", locals())
 	return output
 
+print_diff_globals()
 t = draw_triangle(10)
+print_diff_globals()
 print('\n'.join(t))
+print_diff_globals()
